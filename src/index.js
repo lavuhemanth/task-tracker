461a4d7e030f2c64b7c85c6b6d67e90467fa53b5
HemanthKumarLavu <hemanthkumar.lavu@vasudhaika.net>
2020-06-09 23:06:21 +0530

    added fire base

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAep0ePNQobQg4kHssPREfsY46W5vHiaiE",
+  authDomain: "task-tracker-86e5f.firebaseapp.com",
+  databaseURL: "https://task-tracker-86e5f.firebaseio.com",
+  projectId: "task-tracker-86e5f",
+  storageBucket: "task-tracker-86e5f.appspot.com",
+  messagingSenderId: "672035833630",
+  appId: "1:672035833630:web:3a0f21c23cf3bfba67ab0f",
+  measurementId: "G-VJ1RGQBH7J"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.hydrate(
   <React.StrictMode>
